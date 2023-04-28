4eed466b56f51590caa873abb07b5572cf1a0fa3
svelout <stepanvorobev10@MAIL.RU>
2023-04-28 22:01:28 +0300

    1

---
+++ SCP-217/SCP-217/EventHandlers.cs
@@ -0,0 +1,53 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Scp914;
+using UnityEngine;
+using Random = System.Random;
+using Object = UnityEngine.Object;
+using Exiled.CustomRoles.API.Features;
+using PlayerRoles;
+using System.Collections.Generic;
+
+namespace SCP_217
+{
+    public class EventHandlers
+    {
+        public void OnUpdaradingPlayer(UpgradingPlayerEventArgs ev)
+        {
+            var r = new Random();
+            var ran_num = (double)r.NextDouble() * 100;
+            if (ran_num <= Plugin.Instance.Config.InfectionChance && ev.Player.Role.Side != Side.Scp && ev.Player.GameObject.GetComponent<Infection>() == null)
+            {
+                ev.Player.GameObject.AddComponent<Infection>();
+                Log.Info("Succesfull added component to " + ev.Player.Nickname);
+            }
+        }
+
+        public void OnPlayerDied(DiedEventArgs ev)
+        {         
+            if (ev.Player.GameObject.GetComponent<Infection>() != null)
+            {
+                Object.Destroy(ev.Player.GameObject.GetComponent<Infection>());
+            }
+        }
+
+        public void OnChangingRole(ChangingRoleEventArgs ev)
+        {
+            var crole = CustomRole.Get(3);        
+            if (ev.Player.Role == crole.Role && ev.Player.GameObject.GetComponent<RoleComponent>() != null)
+            {
+                Object.Destroy(ev.Player.GameObject.GetComponent<RoleComponent>());
+            }
+        }
+
+        public void OnPlayerUsedItem(UsedItemEventArgs ev)
+        {
+            if (ev.Item.Type == ItemType.SCP207 && ev.Player.GameObject.GetComponent<Infection>() != null)
+            {
+                Object.Destroy(ev.Player.GameObject.GetComponent<Infection>());
+                ev.Player.Health = 100;
+            }
+        }
+    }
+}
---
+++ SCP-217/SCP-217/Translations.cs
@@ -0,0 +1,16 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCP_217
+{
+    public class Translations : ITranslation
+    {
+        [Description("Format of Hint")]
+        public string WarningHintMessage { get; set; } = "<size=35%><color=(COLOR)><b>(HINT)</b></color></size>";
+    }
+}
